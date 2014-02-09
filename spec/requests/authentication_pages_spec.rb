543a872f9cbee2f24a133d94c460e6572fe62d1a
Harbral <harbral@gmail.com>
2014-02-09 10:30:55 -0800

    Added some stuff from exercises (not all)

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -5,6 +5,8 @@ describe "Authentication" do
   
   describe "signin page" do
 	before { visit signin_path }
+	it { should_not have_content('Profile') }
+	it { should_not have_content('Settings') }
 	
 	describe "with invalid information" do
 		before { click_button "Sign in" }
@@ -22,11 +24,7 @@ describe "Authentication" do
 	
 	describe "with valid information" do
 		let(:user) { FactoryGirl.create(:user) }
-		before do
-			fill_in "Email",	with: user.email.upcase
-			fill_in "Password",	with: user.password
-			click_button "Sign in"
-		end
+		before { sign_in user }
 		
 		it { should have_title(user.name) }
 		it { should have_link('Users', 		href: users_path) }
